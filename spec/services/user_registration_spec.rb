0ba32250aef7944a39886fcba8072c7741668193
Carlos Vilhena <carlosvilhena@gmail.com>
2016-02-07 14:50:17 +0000

    Storing email address from registration/login
    This change makes sure a user is created with its email address from the auth
    hash; furthermore, it also sets the scope of the omniauth github provider so we
    can get the user's email address out of it.

---
+++ spec/services/user_registration_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+require 'ohm'
+require 'active_support/core_ext/time'
+require 'app/models/base_model'
+require 'app/models/user'
+require 'app/services/user_registration'
+
+RSpec.describe UserRegistration do
+  subject { described_class.new(auth_hash) }
+
+  describe '#user' do
+    let(:user) { subject.user }
+
+    context 'with a new user' do
+      let(:email) { 'john@doe.com' }
+      let(:name) { 'John' }
+      let(:provider) { 'github' }
+      let(:uid) { '1' }
+      let(:auth_hash) {
+        {
+          'provider' => provider,
+          'uid' => uid,
+          'info' => {
+            'name' => name,
+            'email' => email
+          }
+        }
+      }
+
+      it 'assigns a uid to the user' do
+        expect(user.uid).to eq([provider, uid].join('|'))
+      end
+
+      it 'assigns the email address to the user' do
+        expect(user.email).to eq(email)
+      end
+
+      it 'assigns the name to the user' do
+        expect(user.name).to eq(name)
+      end
+    end
+  end
+end
