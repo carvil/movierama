a546af123c78566c50294e81e68b1f961bb76422
Carlos Vilhena <carlosvilhena@gmail.com>
2016-02-07 17:38:54 +0000

    Send notifications when a new vote happens
    Right after registering the vote, we should trigger the notifications so the
    user gets the email.

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe VotesController, :type => :controller do
+  let!(:user) {
+    User.create(name: 'John', email: 'john@doe.com')
+  }
+
+  before {
+    expect(controller).to receive(:authorize!).and_return(true)
+    expect(controller).to receive(:current_user).and_return(user)
+  }
 
   describe "GET create" do
-    xit "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    let(:vote) { :like }
+    let!(:movie) {
+      Movie.create(
+        title: 'Title', description: 'Description', date: Date.today
+      )
+    }
+
+    it 'calls the notifier service with the movie' do
+      expect_any_instance_of(VotingNotifier).to receive(
+        :notify).with(vote).once
+      get :create, { t: vote.to_s, movie_id: movie.id }
     end
   end
 
@@ -15,5 +31,4 @@ RSpec.describe VotesController, :type => :controller do
       expect(response).to have_http_status(:success)
     end
   end
-
 end
